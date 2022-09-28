7d8917a2c93627c69e77ec626a43694a288a161f
yangchunbaixue <20111350626@qq.com>
2022-09-28 16:42:03 +0800

    Initial commit

---
+++ src/api/hosp.js
@@ -0,0 +1,41 @@
+import request from '@/utils/request'
+
+export default {
+  //医院列表
+  getPageList(page,limit,searchObj) {
+    return request ({
+      url: `/admin/hosp/hospital/list/${page}/${limit}`,
+      method: 'get',
+      params: searchObj
+    })
+  },
+  //查询dictCode查询下级数据字典
+  findByDictCode(dictCode) {
+    return request({
+      url: `/admin/cmn/dict/findByDictCode/${dictCode}`,
+      method: 'get'
+    })
+  },
+
+  //根据id查询下级数据字典
+  findByParentId(dictCode) {
+    return request({
+      url: `/admin/cmn/dict/findChildData/${dictCode}`,
+      method: 'get'
+    })
+  },
+
+  updateStatus(id,status) {
+    return request ({
+      url: `/admin/hosp/hospital/updateStatus/${id}/${status}`,
+      method: 'get'
+    })
+  },
+  // 查看医院详细
+   getHospById(id){
+    return({
+      url: `/admin/hosp/hospital/show/${id}`,
+      method: 'get'
+    })
+  }
+}
